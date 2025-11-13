e18ad16cc5c9ec115423bed833d074d5da4aae43
Ryan Welcher <me@ryanwelcher.com>
2025-11-13 11:41:07 -0500

    It\s good enough for now

---
+++ plugin/src/mastering-iapi/render.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * PHP file to use when rendering the block type on the server to show on the front end.
+ *
+ * The following variables are exposed to the file:
+ *     $attributes (array): The block attributes.
+ *     $content (string): The block default content.
+ *     $block (WP_Block): The block instance.
+ *
+ * @see https://github.com/WordPress/gutenberg/blob/trunk/docs/reference-guides/block-api/block-metadata.md#render
+ */
+
+$state = array(
+	'copyButtonText' => 'Copy',
+	'activeSnippet'  => 'asdfasd',
+);
+wp_interactivity_state( 'mastering-iapi', $state );
+
+$config = wp_interactivity_config( 'mastering-iapi-code-snippets' );
+?>
+<div
+	<?php echo wp_kses_data( get_block_wrapper_attributes( [ 'class' => 'code-block-wrapper' ] ) ); ?>
+	data-wp-interactive="mastering-iapi"
+	data-wp-init="callbacks.init"
+>
+	<button
+		class="copy-button"
+		data-wp-on--click="actions.copyCode"
+		data-wp-text="state.copyButtonText"
+		hidden
+	></button>
+	<pre class="language-php"><code
+			data-wp-init="callbacks.initCodeBlock"
+			data-wp-text="state.activeSnippet"
+			class="language-php"
+			></code>
+	</pre>
+</div>
---
+++ plugin/src/mastering-iapi/view.js
@@ -0,0 +1,77 @@
+/**
+ * External Dependencies
+ */
+import Prism from 'prismjs';
+
+/**
+ * WordPress Dependencies
+ */
+import { store, getConfig, getElement } from '@wordpress/interactivity';
+
+const { state } = store( 'mastering-iapi', {
+	state: {
+		get noSnippet() {
+			return state.activeSnippet === '';
+		},
+
+		get isPHPSnippet() {
+			return state.lang === 'php';
+		},
+		get isJSSnippet() {
+			return state.lang === 'js';
+		},
+	},
+	actions: {
+		updateCodeSnippet: () => {
+			const el = getElement();
+			const snippet = el?.attributes?.[ 'data-snippet' ];
+			const lang = el?.attributes?.[ 'data-lang' ];
+			const snippets = getConfig( 'mastering-iapi-code-snippets' );
+
+			const code = snippets[ snippet ][ lang ] || '';
+
+			state.lang = lang;
+
+			// console.log( Prism.languages );
+
+			const html = Prism.highlight(
+				code.trim(),
+				lang === 'jsx' ? Prism.languages.jsx : Prism.languages.php,
+				lang === 'jsx' ? 'jsx' : 'php'
+			);
+			console.log( html );
+			state.codeTagRef.innerHTML = html;
+			// Prism.highlightAll();
+			// clean up if the button hasn't reset.
+			state.copyButtonText = 'Copy';
+		},
+
+		copyCode: async () => {
+			try {
+				await navigator.clipboard.writeText( state.activeSnippet );
+				state.copyButtonText = 'Copied!';
+
+				// Reset button text after 2 seconds
+				setTimeout( () => {
+					state.copyButtonText = 'Copy';
+				}, 2000 );
+			} catch ( err ) {
+				console.error( 'Failed to copy code:', err );
+				state.copyButtonText = 'Error';
+				setTimeout( () => {
+					state.copyButtonText = 'Copy';
+				}, 2000 );
+			}
+		},
+	},
+	callbacks: {
+		init: () => {
+			Prism.manual = true;
+			// Prism.highlightAll();
+		},
+		initCodeBlock: () => {
+			const { ref } = getElement();
+			state.codeTagRef = ref;
+		},
+	},
+} );

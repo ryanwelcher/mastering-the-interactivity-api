bbf94981b52062f6627f4674cbe3b7ce72992d58
Ryan Welcher <me@ryanwelcher.com>
2025-11-17 10:21:17 -0500

    Update theme

--- themes/tt4-dark-mode/functions.php
+++ themes/tt4-dark-mode/functions.php
@@ -9,3 +9,21 @@
  * @license   https://www.gnu.org/licenses/gpl-3.0.html GPL-3.0-or-later
  * @link      https://github.com/justintadlock/tt4-dark-mode
  */
+
+/**
+ * Adds a CSS custom property to :root that controls the color-scheme
+ * preference for the entire document. This allows the browser to adjust
+ * default colors and form controls based on the user's preference.
+ */
+add_action(
+	'wp_head',
+	function () {
+		?>
+		<style>
+			:root {
+				color-scheme: light dark;
+			}
+		</style>
+		<?php
+	}
+);

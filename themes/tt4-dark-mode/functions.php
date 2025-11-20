50cd7d5697d65fcf00d8f2495ec4fb0b7ac6fc59
Ryan Welcher <me@ryanwelcher.com>
2025-11-20 12:01:31 -0500

    Theme updates to centralize the css

--- themes/tt4-dark-mode/functions.php
+++ themes/tt4-dark-mode/functions.php
@@ -27,3 +27,12 @@ add_action(
 		<?php
 	}
 );
+
+add_action( 'wp_enqueue_scripts', 'grand_sunrise_enqueue_styles' );
+
+function grand_sunrise_enqueue_styles() {
+	wp_enqueue_style(
+		'grand-sunrise-style',
+		get_stylesheet_uri()
+	);
+}
